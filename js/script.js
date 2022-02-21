e95e8703c85b50d885daea34a073db8583000976
Michelle Buyn <michellebuyn@Michelles-MacBook-Pro.local>
2022-02-21 15:50:43 +1300

    fixed mobile issues and started changing format

--- js/script.js
+++ js/script.js
@@ -8,41 +8,17 @@ $(document).ready(function() {
     }).on("mouseout", function() {
         $(".img_michelle").hide()
     }),
+    $(".link_tauranga").on("mouseover", function() {
+        $(".img_tauranga").show()
+    }).on("mouseout", function() {
+        $(".img_tauranga").hide()
+    }),
     $(".link_projects").on("mouseover", function() {
         $(".img_projects").show()
     }).on("mouseout", function() {
         $(".img_projects").hide()
     }),
-    $(".link_cat").on("mouseover", function() {
-        $(".img_cat").show()
-    }).on("mouseout", function() {
-        $(".img_cat").hide()
-    }),
-    $(".link_film").on("mouseover", function() {
-        $(".img_film").show()
-    }).on("mouseout", function() {
-        $(".img_film").hide()
-    }),
-    $(".link_graphics").on("mouseover", function() {
-        $(".img_graphics").show()
-    }).on("mouseout", function() {
-        $(".img_graphics").hide()
-    }),
-    $(".link_google").on("mouseover", function() {
-        $(".img_google").show()
-    }).on("mouseout", function() {
-        $(".img_google").hide()
-    }),
-    $(".link_moviehunt").on("mouseover", function() {
-        $(".img_moviehunt").show()
-    }).on("mouseout", function() {
-        $(".img_moviehunt").hide()
-    }),
-    $(".link_qsc").on("mouseover", function() {
-        $(".img_qsc").show()
-    }).on("mouseout", function() {
-        $(".img_qsc").hide()
-    })
+
 
 
     /* -------------------------------------------------------------------------- */
